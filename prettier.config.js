ebceb61419ce0a2d081560c3b6687c2def2066d2
Act-Aks <akashsrivastava.git@gmail.com>
2024-06-16 17:52:22 +0530

    feat: 🎸 first commit

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,12 @@
+/* @type {import('prettier').Config} */
 module.exports = {
   printWidth: 100,
   tabWidth: 2,
   singleQuote: true,
   bracketSameLine: true,
-  trailingComma: 'es5',
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  semi: false,
+  endOfLine: 'lf',
 };
